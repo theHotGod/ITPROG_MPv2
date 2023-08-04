c396b403f89fdd5df7cbdeae4c189baec9035b4c
Villy15 <adrianvill07@gmail.com>
2023-08-04 21:04:30 +0800

    test

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const path = require('path');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "p@ssword",
   database: "dbclient_side",
-  port: 3307
+  port: 3306
 });
 
 con.connect(function(err) {
@@ -113,10 +113,82 @@ app.get('/checkout', (req, res) => {
 
   console.log('get checkout: ', cartItems);
 
-  res.render('checkout', {
-    cart: cartItems
-  })
+  const mainDishName = cartItems.find(item => item.category === 'Mains')?.name;
+  const sideDishName = cartItems.find(item => item.category === 'Sides')?.name;
+  const drinkDishName = cartItems.find(item => item.category === 'Drinks')?.name;
+
+  // console.log('mainDishName: ', mainDishName);
+  // console.log('sideDishName: ', sideDishName);
+  // console.log('drinkDishName: ', drinkDishName);
+
+  discountCheck(mainDishName, sideDishName, drinkDishName, (error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const comboName = result.comboName;
+      const discount = result.discount;
+
+      // console.log('comboName: ', comboName);
+      // console.log('discount: ', discount);
+
+      const formattedDiscount = discount ? (discount * 100).toFixed(2) + '%' : null;
+      const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+      const totalPriceAfterDiscount = discount ? (totalPrice * (1 - discount)).toFixed(2) : totalPrice;
+
+      console.log('formattedDiscount: ', formattedDiscount);
+      console.log('totalPrice: ', totalPrice);
+      console.log('totalPriceAfterDiscount: ', totalPriceAfterDiscount);
+
+
+      res.render('checkout', {
+        cart: cartItems,
+        comboName: discount ? comboName : null,
+        discount: formattedDiscount,
+        totalPrice: totalPrice.toFixed(2),
+        totalPriceAfterDiscount: discount ? totalPriceAfterDiscount : null,
+      });
+    }
+  });
 });
+
+function discountCheck(mainDish, sideDish, drink, callback) {
+  const cart = [mainDish, sideDish, drink];
+
+  const comboQuery = "SELECT * FROM combo_content INNER JOIN food_combo ON combo_content.comboID=food_combo.comboID";
+  con.query(comboQuery, (error, comboResults) => {
+    if (error) throw error;
+
+    let comboObj = {
+      comboName: null,
+      discount: 0.00,
+    };
+
+    for (const comboResult of comboResults) {
+      let isComboMatch = true;
+
+      const comboItems = comboResults
+        .filter((result) => result.comboID === comboResult.comboID)
+        .map((result) => result.foodName);
+
+        // console.log('comboItems: ', comboItems);
+
+      for (const comboItem of comboItems) {
+        if (!cart.includes(comboItem)) {
+          isComboMatch = false;
+          break;
+        }
+      }
+
+      if (isComboMatch) {
+        comboObj.comboName = comboResult.comboName;
+        comboObj.discount = comboResult.discount;
+        break; // Exit the loop once a valid combo is found
+      }
+    }
+
+    callback(null, comboObj);
+  });
+}
 
 
 
@@ -135,6 +207,5 @@ app.get('/login', (req, res) => {
 app.listen(port, () => {
     console.log('running');
 });
-
 
 module.exports = con;
