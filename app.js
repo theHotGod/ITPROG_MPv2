3d6267c992bb054c3cd562042ad443cf182dfc9d
Villy15 <adrianvill07@gmail.com>
2023-08-05 17:24:58 +0800

    Add discount db

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var con = mysql.createConnection({
   port: 3306
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
@@ -27,7 +27,7 @@ app.use(express.static('public'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.engine('hbs', exphbs.engine({
-    extname: 'hbs'
+  extname: 'hbs'
 }));
 
 app.use(session({
@@ -38,7 +38,7 @@ app.use(session({
 
 
 app.get('/', (req, res) => {
-    const getDishMain = `SELECT * 
+  const getDishMain = `SELECT * 
                          FROM dish
                          ORDER BY
                          CASE 
@@ -47,15 +47,15 @@ app.get('/', (req, res) => {
                             WHEN dishCategory = 'Drinks' THEN 3
                             ELSE 4
                         END ASC`;
-    con.query(getDishMain, (err, result) => {
-      if (err)
-        throw err;
-      //console.log(result);
-      res.render('home', {
-        dish: result
-      });
+  con.query(getDishMain, (err, result) => {
+    if (err)
+      throw err;
+    //console.log(result);
+    res.render('home', {
+      dish: result
     });
-    
+  });
+
 });
 
 app.post('/addCart', (req, res) => {
@@ -142,39 +142,33 @@ app.get('/checkout', (req, res) => {
       console.log('totalPriceAfterDiscount: ', totalPriceAfterDiscount);
 
 
-      // Add totalPrice to the session when there is no discount
-      if (!discount) {
-        req.session.totalPrice = totalPrice;
-      } else {
-        // If there is a discount, make sure totalPrice is not present in the session
-        delete req.session.totalPrice;
-      }
 
+      req.session.totalPrice = totalPrice;
       req.session.totalPriceAfterDiscount = discount ? totalPriceAfterDiscount : null;
 
 
       res.render('checkout', {
-    cart: cartItems,
-    comboName: discount ? comboName : null,
-    discount: formattedDiscount,
-    totalPrice: totalPrice.toFixed(2),
-    totalPriceAfterDiscount: discount ? totalPriceAfterDiscount : null,
-  });
+        cart: cartItems,
+        comboName: discount ? comboName : null,
+        discount: formattedDiscount,
+        totalPrice: totalPrice.toFixed(2),
+        totalPriceAfterDiscount: discount ? totalPriceAfterDiscount : null,
+      });
     }
   });
 });
 
 app.post('/cancel', (req, res) => {
   req.session.destroy((err) => {
-      if (err) {
-          console.error(err);
-          res.status(500).send('Failed to cancel the order');
-      } else {
-        console.log('Order canceled successfully');
-        res.sendStatus(200);
-      }
+    if (err) {
+      console.error(err);
+      res.status(500).send('Failed to cancel the order');
+    } else {
+      console.log('Order canceled successfully');
+      res.sendStatus(200);
+    }
   });
-}); 
+});
 
 app.post('/submitOrder', (req, res) => {
   const cartItems = req.session.cartItems || [];
@@ -190,22 +184,28 @@ app.post('/submitOrder', (req, res) => {
   if (totalPriceAfterDiscount) {
     // If there is a discount, we already have totalPriceAfterDiscount in the session
     // So, no need to recalculate it
-    delete req.session.totalPriceAfterDiscount;
+    // delete req.session.totalPriceAfterDiscount;
+    totalPrice = req.session.totalPrice;
   } else {
     // If there is no discount, retrieve the totalPrice from the session
     totalPrice = req.session.totalPrice;
-    delete req.session.totalPrice; // Remove totalPrice from the session
+    // delete req.session.totalPrice; // Remove totalPrice from the session
   }
+
+  let totalDiscount = totalPriceAfterDiscount ? totalPrice - totalPriceAfterDiscount : 0;
+
+
 
   console.log("totalPriceAfterDiscount: ", totalPriceAfterDiscount);
   console.log("totalPrice: ", totalPrice);
   console.log("custName: ", custName);
+  console.log("totalDiscount: ", totalDiscount);
 
   const priceToInsert = totalPriceAfterDiscount || totalPrice;
 
   // Insert order into the 'orders' table
   const date = new Date();
-  con.query("INSERT INTO orders (totalPrice, orderBy, orderedAt) VALUES (?, ?, ?)", [totalPriceAfterDiscount || totalPrice, custName, date], (err, result) => {
+  con.query("INSERT INTO orders (totalPrice, discountedPrice, orderBy, orderedAt) VALUES (?, ?, ?, ?)", [totalPriceAfterDiscount || totalPrice, totalDiscount, custName, date], (err, result) => {
     if (err) throw err;
 
     // Get the last inserted order ID
@@ -230,13 +230,56 @@ app.post('/submitOrder', (req, res) => {
     console.log("Order data inserted!");
 
     // Clear the cart after successful order submission
-    req.session.cartItems = [];
+    // req.session.cartItems = [];
 
     // Redirect to the success page or any other page as desired
     res.redirect('/success');
   });
 });
 
+app.get('/success', (req, res) => {
+  const cartItems = req.session.cartItems || [];
+  const totalPrice = req.session.totalPrice || 0;
+  const totalPriceAfterDiscount = req.session.totalPriceAfterDiscount || null;
+
+  const mainDishName = cartItems.find(item => item.category === 'Mains')?.name;
+  const sideDishName = cartItems.find(item => item.category === 'Sides')?.name;
+  const drinkDishName = cartItems.find(item => item.category === 'Drinks')?.name;
+
+  discountCheck(mainDishName, sideDishName, drinkDishName, (error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const comboName = result.comboName;
+      const discount = result.discount;
+
+      const formattedDiscount = discount ? (discount * 100) + '%' : null;
+      const totalPriceAfterDiscountFormatted = totalPriceAfterDiscount !== null ? totalPriceAfterDiscount : null;
+
+      console.log("SUCCESS");
+      console.log('cart: ', cartItems);
+      console.log("totalPrice: ", totalPrice);
+      console.log("totalPriceAfterDiscount: ", totalPriceAfterDiscount);
+
+      console.log("mainDishName: ", mainDishName);
+      console.log("sideDishName: ", sideDishName);
+      console.log("drinkDishName: ", drinkDishName);
+
+      console.log("comboName: ", comboName);
+      console.log("discount: ", discount);
+      console.log("formattedDiscount: ", formattedDiscount);
+      console.log("totalPriceAfterDiscountFormatted: ", totalPriceAfterDiscountFormatted);
+
+      res.render('success', {
+        cart: cartItems,
+        comboName: discount ? comboName : null,
+        discount: formattedDiscount,
+        totalPrice: totalPrice.toFixed(2),
+        totalPriceAfterDiscount: totalPriceAfterDiscountFormatted,
+      });
+    }
+  });
+});
 
 
 function discountCheck(mainDish, sideDish, drink, callback) {
@@ -258,7 +301,7 @@ function discountCheck(mainDish, sideDish, drink, callback) {
         .filter((result) => result.comboID === comboResult.comboID)
         .map((result) => result.foodName);
 
-        // console.log('comboItems: ', comboItems);
+      // console.log('comboItems: ', comboItems);
 
       for (const comboItem of comboItems) {
         if (!cart.includes(comboItem)) {
@@ -281,19 +324,19 @@ function discountCheck(mainDish, sideDish, drink, callback) {
 
 
 app.get('/main', (req, res) => {
-    res.redirect('/');
+  res.redirect('/');
 });
 
 const viewsFolderPath = path.join(__dirname, 'views');
 
 app.get('/login', (req, res) => {
-    const loginFilePath = path.join(viewsFolderPath, 'login.php');
-    res.set('Content-Type', 'text/html');
-    res.sendFile(loginFilePath);
+  const loginFilePath = path.join(viewsFolderPath, 'login.php');
+  res.set('Content-Type', 'text/html');
+  res.sendFile(loginFilePath);
 });
 
 app.listen(port, () => {
-    console.log('running');
+  console.log('running');
 });
 
 module.exports = con;
